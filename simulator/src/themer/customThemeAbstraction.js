b761383acf7f80f29dab2a3b1bdacc0b3e683940
fillingtothemomo <rockingpenny4@gmail.com>
2024-01-17 18:08:02 +0530

    fix(simulator): adds field to change canvas text color

--- simulator/src/themer/customThemeAbstraction.js
+++ simulator/src/themer/customThemeAbstraction.js
@@ -3,42 +3,46 @@
  * @param {*} themeOptions
  * @returns an Object
  */
-export const CreateAbstraction = (themeOptions) => {
-    return {
-        Navbar: {
-            color: themeOptions['--bg-navbar'],
-            description: 'navbar background',
-            ref: ['--bg-navbar'],
-        },
-        Primary: {
-            color: themeOptions['--primary'],
-            description: 'modals background',
-            ref: ['--primary'],
-        },
-        Secondary: {
-            color: themeOptions['--bg-tabs'],
-            description: 'tabBar background',
-            ref: ['--bg-tabs'],
-        },
-        Canvas: {
-            color: themeOptions['--canvas-fill'],
-            description: 'canvas background',
-            ref: ['--canvas-fill'],
-        },
-        Stroke: {
-            color: themeOptions['--canvas-stroke'],
-            description: 'canvas grid color',
-            ref: ['--canvas-stroke'],
-        },
-        Text: {
-            color: themeOptions['--text-lite'],
-            description: 'text color',
-            ref: ['--text-lite', '--text-panel', '--text-dark'],
-        },
-        Borders: {
-            color: themeOptions['--br-secondary'],
-            description: 'borders color',
-            ref: ['--br-secondary'],
-        },
-    };
-};
+const CreateAbstraction = (themeOptions) => ({
+    Navbar: {
+        color: themeOptions['--bg-navbar'],
+        description: 'navbar background',
+        ref: ['--bg-navbar'],
+    },
+    Primary: {
+        color: themeOptions['--primary'],
+        description: 'modals background',
+        ref: ['--primary'],
+    },
+    Secondary: {
+        color: themeOptions['--bg-tabs'],
+        description: 'tabBar background',
+        ref: ['--bg-tabs'],
+    },
+    Canvas: {
+        color: themeOptions['--canvas-fill'],
+        description: 'canvas background',
+        ref: ['--canvas-fill'],
+    },
+    Stroke: {
+        color: themeOptions['--canvas-stroke'],
+        description: 'canvas grid color',
+        ref: ['--canvas-stroke'],
+    },
+    Text: {
+        color: themeOptions['--text-lite'],
+        description: 'text color',
+        ref: ['--text-lite', '--text-panel', '--text-dark'],
+    },
+    Borders: {
+        color: themeOptions['--br-secondary'],
+        description: 'borders color',
+        ref: ['--br-secondary'],
+    },
+    CanvasText: {
+        color: themeOptions['--text'],
+        description: 'canvas text color',
+        ref: ['--text'],
+    },
+});
+export default CreateAbstraction;
